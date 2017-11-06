ef6c2e5d724aef19a852b8986650d1e4a6d211e5
javierxperez <jperez60@asu.edu>
2017-11-06 15:42:29 -0700

    SocialMediaComponent
    Button that leads to Social Media Activity

---
+++ PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/SocialMediaActivity.java
@@ -0,0 +1,48 @@
+package asu.whirlpool.psychewhirlpool;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SocialMediaActivity extends AppCompatActivity
+{
+    private TextView message;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            switch (item.getItemId())
+            {
+                case R.id.navigation_home:
+                    message.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    message.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    message.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socialmedia);
+
+        message = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
