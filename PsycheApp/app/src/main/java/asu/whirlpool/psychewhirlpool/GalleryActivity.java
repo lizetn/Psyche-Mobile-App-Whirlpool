a62dcb3182e070115bfc2f8bd29e75f50fe63109
FelanScarlet <dfchavez@asu.edu>
2017-11-06 20:09:52 -0700

    GalleryActivity Created
    The GalleryActivity was added along with a button on the main screen.

--- PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/GalleryActivity.java
+++ PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/GalleryActivity.java
@@ -1,16 +1,14 @@
 package asu.whirlpool.psychewhirlpool;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class GalleryActivity extends AppCompatActivity {
+
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -34,53 +32,13 @@ public class MainActivity extends AppCompatActivity
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gallery);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    /**
-     * Navigates to {@link FactsActivity}
-     * @param view
-     */
-    public void displayFacts(View view)
-    {
-        Intent intent = new Intent(this, FactsActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * Navigates to {@link TimelineActivity}
-     * @param view
-     */
-    public void displayTimeline(View view)
-    {
-        Intent intent = new Intent(this, TimelineActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * Navigates to {@link SocialMediaActivity}
-     * @param view
-     */
-    public void displaySocialMedia(View view)
-    {
-        Intent intent = new Intent(this, SocialMediaActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * Navigates to {@link GalleryActivity}
-     * @param view
-     */
-    public void displayGallery(View view)
-    {
-        Intent intent = new Intent(this, GalleryActivity.class);
-        startActivity(intent);
-    }
 }
