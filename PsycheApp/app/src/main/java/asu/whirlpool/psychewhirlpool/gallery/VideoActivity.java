fffdfd1e1fe9f8645538e467d3cb9ccc48da1181
Erick Ramirez Cordero <rami7535@gmail.com>
2018-02-04 13:54:12 -0700

    Update to VideoActivity

--- PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/gallery/VideoActivity.java
+++ PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/gallery/VideoActivity.java
@@ -12,10 +12,34 @@ import asu.whirlpool.psychewhirlpool.R;
  */
 public class VideoActivity extends AppCompatActivity
 {
+    public int ResourceId;
+    private MediaInterface videoPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        // Initialize Video Player
+        videoPlayer = new VideoPlayer(this);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        videoPlayer.load(ResourceId);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+
+        if (isChangingConfigurations() && videoPlayer.isPlaying())
+        {
+            //
+        }
     }
 }
