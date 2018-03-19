9b1279788ea0facc1abe26d9e3d870a888de9cfa
FelanScarlet <dfchavez@asu.edu>
2018-03-18 20:25:56 -0700

    ListView Updates
    ListView updated to include background images in every section. Contents
    of listview updated so there is a background image and a title.

---
+++ PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/facts/ListViewAdapter.java
@@ -0,0 +1,68 @@
+package asu.whirlpool.psychewhirlpool.facts;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import asu.whirlpool.psychewhirlpool.R;
+
+/**
+ * Created by Diana on 3/18/2018.
+ */
+
+public class ListViewAdapter extends BaseAdapter {
+
+    String[] faqoptions;
+    LayoutInflater layoutListView;
+    int[] faqImages = {
+            R.drawable.game_icon, R.drawable.game_icon,
+            R.drawable.game_icon, R.drawable.game_icon,
+            R.drawable.game_icon, R.drawable.game_icon,
+            R.drawable.game_icon, R.drawable.game_icon,
+            R.drawable.game_icon, R.drawable.game_icon,
+            R.drawable.game_icon, R.drawable.game_icon,
+    };
+
+    public ListViewAdapter(Context context) {
+        layoutListView = LayoutInflater.from(context);
+        faqoptions = context.getResources().getStringArray(R.array.faq_options);
+    }
+
+    @Override
+    public int getCount(){
+        return faqoptions.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View view = convertView;
+
+        if(view == null){
+            view = layoutListView.inflate(R.layout.facts_listview, parent, false);
+        }
+
+        TextView faqTitles = view.findViewById(R.id.titleListView);
+        ImageView faqImageView = view.findViewById(R.id.background);
+
+        faqTitles.setText(faqoptions[position]);
+        faqImageView.setImageResource(faqImages[position]);
+
+        return view;
+    }
+}
