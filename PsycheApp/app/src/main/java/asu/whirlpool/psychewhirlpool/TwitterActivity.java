440f1bb1b11358cb6b48e518b5e07514e7dc6424
Erick Ramirez Cordero <rami7535@gmail.com>
2018-03-28 03:59:04 -0700

    Update to Android Studio 3.1
    Updating to 3.1 caused some compile issues. This should fix those issues
    but needs to be verified by another to make sure.

--- PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/TwitterActivity.java
+++ PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/TwitterActivity.java
@@ -7,50 +7,52 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-//import io.fabric.sdk.android.Fabric;
-//import com.twitter.sdk.android.core.Callback;
-//import com.twitter.sdk.android.core.Result;
-//import com.twitter.sdk.android.core.TwitterCore;
-//import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.Twitter;
 import android.os.CountDownTimer;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 
-
-public class TwitterActivity extends ListFragment {
+/**
+ * TODO: Add documentation
+ */
+public class TwitterActivity extends ListFragment
+{
     CountDownTimer mCountDownTimer;
     String mp = "";
     View view;
 
+    /**
+     * ???
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.activity_socialmedia, container, false);
-        final TextView ls = (TextView) view.findViewById(R.id.android_ls);
+        final TextView ls = view.findViewById(R.id.ls);
         ls.setText("");
 
-        CountdownActivity ms = new CountdownActivity();
-        mCountDownTimer = new CountDownTimer(8000, 1000) {
+        mCountDownTimer = new CountDownTimer(8000, 1000)
+        {
             @Override
-            public void onTick(long l) {
+            public void onTick(long l)
+            {
                 Long countdownSeconds = l / 1000;
                 mp = String.format("%d", ((countdownSeconds % 86400) % 3600) % 60);
                 if(countdownSeconds == 1 || countdownSeconds == 1000)
                 {
-                    ls.setText("Check internet connection");
+                    ls.setText(getString(R.string.internet_error));
                     mCountDownTimer.cancel();
                 }
             }
 
             @Override
-            public void onFinish() {
+            public void onFinish()
+            {
                 mCountDownTimer.cancel();
             }
         };
 
         mCountDownTimer.start();
-
         Twitter.initialize(view.getContext());
 
         final UserTimeline userTimeline = new UserTimeline.Builder()
