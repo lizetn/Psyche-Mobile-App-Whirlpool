9661d55021796cf66ca401c42fd9baabe3f5a54e
FelanScarlet <dfchavez@asu.edu>
2018-04-15 19:17:54 -0700

    MissionFact Changes
    Updates to the TextColor and XML

--- PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/facts/FactContentActivity.java
+++ PsycheApp/app/src/main/java/asu/whirlpool/psychewhirlpool/facts/FactContentActivity.java
@@ -3,8 +3,10 @@ package asu.whirlpool.psychewhirlpool.facts;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
@@ -25,7 +27,7 @@ public class FactContentActivity extends AppCompatActivity
 {
     private TypedArray contentArray;
     private TextView contentTextView;
-    private ImageView missionBackground;
+    private ConstraintLayout missionBackground;
 
     private int[] backgroundDayImages = {R.drawable.tab_background_light1, R.drawable.tab_background_light2,
             R.drawable.tab_background_light3, R.drawable.tab_background_light4,
@@ -61,15 +63,31 @@ public class FactContentActivity extends AppCompatActivity
         // Get the FAQ key which denotes which section of the FAQ was selected.
         Intent intent = getIntent();
         int position = intent.getExtras().getInt(FactsActivity.FAQ_KEY);
-        missionBackground = findViewById(R.id.backgroundImageView);
+        missionBackground = findViewById(R.id.ConstraintBackground);
+
+        final int sdk = android.os.Build.VERSION.SDK_INT;
 
         if (nightMode)
         {
-            missionBackground.setImageResource(backgroundNightImages[position]);
+            if(sdk >= android.os.Build.VERSION_CODES.JELLY_BEAN)
+            {
+                missionBackground.setBackground(ContextCompat.getDrawable(this, backgroundNightImages[position]));
+            }
+            else
+            {
+                missionBackground.setBackgroundDrawable(ContextCompat.getDrawable(this, backgroundNightImages[position]));
+            }
         }
         else
         {
-            missionBackground.setImageResource(backgroundDayImages[position]);
+            if(sdk >= android.os.Build.VERSION_CODES.JELLY_BEAN)
+            {
+                missionBackground.setBackground(ContextCompat.getDrawable(this, backgroundDayImages[position]));
+            }
+            else
+            {
+                missionBackground.setBackgroundDrawable(ContextCompat.getDrawable(this, backgroundDayImages[position]));
+            }
         }
 
         contentArray = getResources().obtainTypedArray(R.array.faq_array);
